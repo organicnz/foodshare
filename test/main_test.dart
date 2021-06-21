dcfd1deb37695a0269cb2a3beb9f0f4ad4504134
organicnz <tamerlanium@gmail.com>
2021-06-21 10:31:04 +0300

    Add CI jobs for building Android and iOS

---
+++ test/main_test.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:foodshare/main.dart';
+
+// void main() {
+//   testWidgets('MyApp displays name', (WidgetTester tester) async {
+//     await tester.pumpWidget(MyApp(name: 'John Smith'));
+
+//     final nameFinder = find.text('John Smith');
+
+//     expect(nameFinder, findsOneWidget);
+//   });
+// }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,41 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// import 'package:test/test.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// void main() {
+//   group('Notes Cubit', () {
+//     test('default is empty', () {
+//       var cubit = NotesCubit();
+//       expect(cubit.state.notes, []);
+//     });
 
-import 'package:foodshare/main.dart';
+//     test('add notes', () {
+//       var title = 'title';
+//       var body = 'body';
+//       var cubit = NotesCubit();
+//       cubit.createNote(title, body);
+//       expect(cubit.state.notes.length, 1);
+//       expect(cubit.state.notes.first, Note(1, title, body));
+//     });
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+//     test('delete notes', () {
+//       var cubit = NotesCubit();
+//       cubit.createNote('title', 'body');
+//       cubit.createNote('another title', 'another body');
+//       cubit.deleteNote(1);
+//       expect(cubit.state.notes.length, 1);
+//       expect(cubit.state.notes.first.id, 2);
+//     });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     test('update notes', () {
+//       var cubit = NotesCubit();
+//       cubit.createNote('title', 'body');
+//       cubit.createNote('another title', 'another body');
+//       cubit.createNote('yet another title', 'yet another body');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//       var newTitle = 'my cool note';
+//       var newBody = 'my cool note body';
+//       cubit.updateNote(2, newTitle, newBody);
+//       expect(cubit.state.notes.length, 3);
+//       expect(cubit.state.notes[1], Note(2, newTitle, newBody));
+//     });
+//   });
+// }
